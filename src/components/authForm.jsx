0744c91930866afe4590f7ca0971258433b40cb2
09Deep <deepips09@gmail.com>
2025-03-22 19:55:07 -0500

    forgot /api earlier
    /api added

--- src/components/authForm.jsx
+++ src/components/authForm.jsx
@@ -16,7 +16,7 @@ const AuthForm = ({ onLogin }) => {
 		setError(""); // Reset errors before making a request
 
 		try {
-			const response = await fetch("http://127.0.0.1:5000/api/auth/login", {
+			const response = await fetch("https://serverfoodiego.onrender.com/api/auth/login", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({ email, password }),
